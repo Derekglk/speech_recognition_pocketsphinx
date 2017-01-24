e1a8e09a95649d288157b347b0fe8665068ad47a
uestclx <uestclx@gmail.com>
2017-01-24 16:20:50 +0100

    adapt pocketsphinx's keyword detection function

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
 #include <pocketsphinx.h>
+#include <sys/select.h>
+#include <sphinxbase/err.h>
+#include <sphinxbase/ad.h>
 
+#if 0
 int
 main(int argc, char *argv[])
 {
@@ -20,7 +27,7 @@ main(int argc, char *argv[])
         fprintf(stderr, "Failed to create config object, see log for details\n");
         return -1;
     }
-    
+
     ps = ps_init(config);
     if (ps == NULL) {
         fprintf(stderr, "Failed to create recognizer, see log for details\n");
@@ -34,13 +41,13 @@ main(int argc, char *argv[])
     }
 
     rv = ps_start_utt(ps);
-    
+
     while (!feof(fh)) {
         size_t nsamp;
         nsamp = fread(buf, 2, 512, fh);
         rv = ps_process_raw(ps, buf, nsamp, FALSE, FALSE);
     }
-    
+
     rv = ps_end_utt(ps);
     hyp = ps_get_hyp(ps, &score);
     printf("Recognized: %s\n", hyp);
@@ -48,6 +55,160 @@ main(int argc, char *argv[])
     fclose(fh);
     ps_free(ps);
     cmd_ln_free_r(config);
-    
+
+    return 0;
+}
+#endif
+
+static ps_decoder_t *ps;
+static cmd_ln_t *config;
+
+static const arg_t cont_args_def[] = {
+    POCKETSPHINX_OPTIONS,
+    /* Argument file. */
+    {"-argfile",
+     ARG_STRING,
+     NULL,
+     "Argument file giving extra arguments."},
+    {"-adcdev",
+     ARG_STRING,
+     NULL,
+     "Name of audio device to use for input."},
+    {"-infile",
+     ARG_STRING,
+     NULL,
+     "Audio file to transcribe."},
+    {"-inmic",
+     ARG_BOOLEAN,
+     "no",
+     "Transcribe audio from microphone."},
+    {"-time",
+     ARG_BOOLEAN,
+     "no",
+     "Print word times in file transcription."},
+    CMDLN_EMPTY_OPTION
+};
+
+
+static void
+sleep_msec(int32 ms)
+{
+    /* ------------------- Unix ------------------ */
+    struct timeval tmo;
+
+    tmo.tv_sec = 0;
+    tmo.tv_usec = ms * 1000;
+
+    select(0, NULL, NULL, NULL, &tmo);
+}
+
+
+static void
+recognize_from_microphone()
+{
+    ad_rec_t *ad;
+    int16 adbuf[2048];
+    uint8 utt_started, in_speech;
+    int32 k;
+    char const *hyp;
+
+    if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
+                          (int) cmd_ln_float32_r(config,
+                                                 "-samprate"))) == NULL)
+        E_FATAL("Failed to open audio device\n");
+
+    ps_set_search(ps, "keyword");
+
+    if (ad_start_rec(ad) < 0)
+        E_FATAL("Failed to start recording\n");
+
+    if (ps_start_utt(ps) < 0)
+        E_FATAL("Failed to start utterance\n");
+    utt_started = FALSE;
+    E_INFO("Ready....\n");
+
+    for (;;) {
+        if ((k = ad_read(ad, adbuf, 2048)) < 0)
+            E_FATAL("Failed to read audio\n");
+        ps_process_raw(ps, adbuf, k, FALSE, FALSE);
+        in_speech = ps_get_in_speech(ps);
+        if (in_speech && !utt_started) {
+            utt_started = TRUE;
+            E_INFO("Listening...\n");
+        }
+        if (!in_speech && utt_started) {
+            /* speech -> silence transition, time to start new utterance  */
+            ps_end_utt(ps);
+            hyp = ps_get_hyp(ps, NULL );
+            if (hyp != NULL) {
+                printf("%s\n", hyp);
+                printf("keyphrase = %s\n", cmd_ln_str_r(config, "-keyphrase"));
+                fflush(stdout);
+
+                if (!strcmp(ps_get_search(ps), "keyword")) {
+                    if (!strcmp(hyp, cmd_ln_str_r(config, "-keyphrase"))) {
+                        printf("OK, tres bien. MATCH! Speak the command=====>\n");
+                        ps_set_search(ps, "lm");
+                    }
+                } else if (!strcmp(ps_get_search(ps), "lm")) {
+                    ps_set_search(ps, "keyword");
+                    printf("=========>this is where we do the processing with hyp\n");
+                }
+            }
+
+            if (ps_start_utt(ps) < 0)
+                E_FATAL("Failed to start utterance\n");
+            utt_started = FALSE;
+            E_INFO("Ready....\n");
+        }
+        sleep_msec(100);
+    }
+    ad_close(ad);
+}
+
+
+int
+main(int argc, char *argv[])
+{
+    char const *cfg;
+
+    config = cmd_ln_parse_r(NULL, cont_args_def, argc, argv, TRUE);
+
+    /* Handle argument file as -argfile. */
+    if (config && (cfg = cmd_ln_str_r(config, "-argfile")) != NULL) {
+        config = cmd_ln_parse_file_r(config, cont_args_def, cfg, FALSE);
+    }
+
+    if (config == NULL || (cmd_ln_str_r(config, "-infile") == NULL && cmd_ln_boolean_r(config, "-inmic") == FALSE)) {
+	E_INFO("Specify '-infile <file.wav>' to recognize from file or '-inmic yes' to recognize from microphone.\n");
+        cmd_ln_free_r(config);
+	return 1;
+    }
+
+    ps_default_search_args(config);
+
+    printf("dictionary file = %s\n", cmd_ln_str_r(config, "-dict"));
+    printf("language model = %s\n", cmd_ln_str_r(config, "-lm"));
+    printf("keyphrase = %s\n", cmd_ln_str_r(config, "-keyphrase"));
+    printf("kws_threshold = %8f\n", cmd_ln_float_r(config, "-kws_threshold"));
+
+    ps = ps_init(config);
+    if (ps == NULL) {
+        cmd_ln_free_r(config);
+        return 1;
+    }
+
+    ps_set_keyphrase(ps, "keyword", cmd_ln_str_r(config, "-keyphrase"));
+    ps_set_lm_file(ps, "lm", cmd_ln_str_r(config, "-lm"));
+
+    E_INFO("%s COMPILED ON: %s, AT: %s\n\n", argv[0], __DATE__, __TIME__);
+
+    if (cmd_ln_boolean_r(config, "-inmic")) {
+        recognize_from_microphone();
+    }
+
+    ps_free(ps);
+    cmd_ln_free_r(config);
+
     return 0;
 }
